a962b7e443fd5cc13a7bbd0807f51704f5efa94e
Napat48 <72595749+Napat48@users.noreply.github.com>
2020-10-09 14:51:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ def index():
     try:
         f = open("student.csv", "r")
         for line in f.readlines():
-           print(line)
+            print(line)
             a = line.split(",")
-          if(a[0]=="00001"):
-             return a[4]
+            if(a[0]=="00001"):
+               return a[4]
         f.close()
     except Exception:
         return "Could not read to file"
